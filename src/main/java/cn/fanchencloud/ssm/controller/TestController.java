c503971c315e6210a419b41f3f994974138042c4
尘 <1441088980@qq.com>
2022-06-13 03:05:44 +0800

    集成Spring MVC

--- src/main/java/cn/fanchencloud/ssm/controller/TestController.java
+++ src/main/java/cn/fanchencloud/ssm/controller/TestController.java
@@ -1,5 +1,10 @@
 package cn.fanchencloud.ssm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Version: V1.0    <br/>
  * Datetime:   2022/6/13 1:45   <br/>
@@ -7,5 +12,15 @@ package cn.fanchencloud.ssm.controller;
  *
  * @author: chen
  */
+@Controller
+@RequestMapping("/test")
 public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public ModelAndView test() {
+        ModelAndView modelAndView = new ModelAndView("test");
+        modelAndView.addObject("name", "fanchencloud");
+        modelAndView.getModel().put("age", 18);
+        return modelAndView;
+    }
 }
