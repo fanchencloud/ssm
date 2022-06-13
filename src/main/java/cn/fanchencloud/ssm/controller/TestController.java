6a0214acacd07e096a4beff1a13d8a006d54bf28
尘 <1441088980@qq.com>
2022-06-13 13:33:55 +0800

    整合Spring、Spring MVC

--- src/main/java/cn/fanchencloud/ssm/controller/TestController.java
+++ src/main/java/cn/fanchencloud/ssm/controller/TestController.java
@@ -1,5 +1,7 @@
 package cn.fanchencloud.ssm.controller;
 
+import cn.fanchencloud.ssm.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +18,23 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 public class TestController {
 
+    /**
+     * 测试服务
+     */
+    private TestService testService;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ModelAndView test() {
+        // 执行测试任务
+        testService.test();
         ModelAndView modelAndView = new ModelAndView("test");
         modelAndView.addObject("name", "fanchencloud");
         modelAndView.getModel().put("age", 18);
         return modelAndView;
+    }
+
+    @Autowired
+    public void setTestService(TestService testService) {
+        this.testService = testService;
     }
 }
